f31c35a9f7f9ef260870fdc6ef72db19e7e494ba
gaixixon <gaixixon@gmail.com>
2025-10-31 22:14:09 +0700

    Updates

--- src/pages/Settings.jsx
+++
@@ -1,8 +0,0 @@
-export default function Settings() {
-  return (
-    <div>
-      <h1>⚙️ Settings</h1>
-      <p>Adjust your app preferences here.</p>
-    </div>
-  );
-}
--- vite.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/noteapp/',  // 👈 must match your repository name
-})
